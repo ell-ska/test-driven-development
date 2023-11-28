ff41fa4a86155ae40863b2547088a10a0240ef22
ell-ska <ella@skagerstrom.se>
2023-11-28 14:36:37 +0100

    add wrong guess

---
+++ components/WrongGuess.tsx
@@ -0,0 +1,11 @@
+const WrongGuess = () => {
+  return (
+    <p
+      className='absolute left-4 bottom-4 text-lg bg-slate-900 px-4 py-2 rounded-full'
+    >
+      wrong guess!
+    </p>
+  )
+}
+
+export default WrongGuess
