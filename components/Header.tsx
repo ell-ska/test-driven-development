8f7ab2cae0439fc75b0de03611ebf4b0d8e28f6d
ell-ska <ella@skagerstrom.se>
2023-11-28 13:14:00 +0100

    add header

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="py-4 px-8 flex justify-center">
+      <h1 className="text-xl">color guessing game</h1>
+    </header>
+  )
+}
+
+export default Header
